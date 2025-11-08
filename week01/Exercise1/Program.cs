3b3433794f05852e2d31cd850b72808ac03eeaf3
dariusscotland <dscotland@byupathway.edu>
2025-11-08 19:24:49 -0400

    Input and Output

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("Please enter your first name: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Please enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        string introduction = $"Your name is {lastName}, {firstName}, {lastName}";
+
+        Console.WriteLine("\n--- Agent Protocol Activated ---");
+        Console.WriteLine(introduction);
     }
 }
