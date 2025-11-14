94a3b02e4251b933919391efc840811cc26625c2
dariusscotland <dscotland@byupathway.edu>
2025-11-13 21:58:02 -0400

    Encapsulation

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,60 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction() : this(1, 1)
+    {
+    }
+
+    public Fraction(int wholeNumber) : this(wholeNumber, 1)
+    {
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        if (bottom == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        _bottom = bottom;
+    }
+    
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        // --- Testing Representation Methods (GetFractionString and GetDecimalValue) ---
+        Console.WriteLine("\n--- Testing Representation Methods ---");
+
+        // 1. Test 1/1 (using no-parameter constructor)
+        Fraction fractionOne = new Fraction();
+        Console.WriteLine(fractionOne.GetFractionString());
+        Console.WriteLine(fractionOne.GetDecimalValue());
+
+        // 2. Test 5/1 (using one-parameter constructor)
+        Fraction fractionFive = new Fraction(5);
+        Console.WriteLine(fractionFive.GetFractionString());
+        Console.WriteLine(fractionFive.GetDecimalValue());
+
+        // 3. Test 3/4 (using two-parameter constructor)
+        Fraction fractionThreeFourths = new Fraction(3, 4);
+        Console.WriteLine(fractionThreeFourths.GetFractionString());
+        Console.WriteLine(fractionThreeFourths.GetDecimalValue());
+        
+        // 4. Test 1/3 (using two-parameter constructor)
+        Fraction fractionOneThird = new Fraction(1, 3);
+        Console.WriteLine(fractionOneThird.GetFractionString());
+        Console.WriteLine(fractionOneThird.GetDecimalValue());
     }
 }
