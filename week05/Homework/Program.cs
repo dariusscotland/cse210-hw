515f41eb7411cc178ea30b0e3e0d860d9f127860
dariusscotland <dscotland@byupathway.edu>
2025-11-29 20:23:58 -0400

    Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // 1. Test the base Assignment class
+        Assignment simpleAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(simpleAssignment.GetSummary());
+
+        Console.WriteLine("-------------------------");
+
+        // 2. Test the MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment(
+            "Roberto Rodriguez",
+            "Fractions",
+            "7.3",
+            "8-19"
+        );
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        
+        Console.WriteLine("-------------------------");
+
+        // 3. Test the WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment(
+            "Mary Waters",
+            "European History",
+            "The Causes of World War II"
+        );
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
